1e76eb09140b95be465662283c9ea4399512ff7d
SA <32687682+sajidahakther@users.noreply.github.com>
2018-10-19 06:49:25 +0100

    Create interceptedtext.py

---
+++ interceptedtext.py
@@ -0,0 +1,25 @@
+#To analyze what citizens are saying, built a Naive Bayes classifier that predicts whether an intercepted text is positive/negative
+#Using supervised learning, based on the frequency that a word is used in a good training example or a bad one.
+
+from texts import text_counter, text_training
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+intercepted_text = "This is really cool."
+
+text_counts = text_counter.transform([intercepted_text])
+
+text_classifier = MultinomialNB()
+
+text_labels = [0] * 1000 + [1] * 1000
+
+text_classifier.fit(text_training, text_labels)
+
+final_pos = text_classifier.predict_proba(text_counts)[0][1]
+
+final_neg = text_classifier.predict_proba(text_counts)[0][0]
+
+if final_pos > final_neg:
+  print("The text is positive.")
+else:
+  print("The text is negative.")
